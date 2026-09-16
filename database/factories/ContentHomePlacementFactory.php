05d0f2bba6a412017adb527a4de29a8b1a5c7776
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-16 06:11:09 +0200

    test: add ContentHomePlacementFactory

---
+++ database/factories/ContentHomePlacementFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ContentArticle;
+use App\Models\ContentHomePlacement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ContentHomePlacement>
+ */
+class ContentHomePlacementFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'surface_key' => 'newsroom_home',
+            'slot_key' => fake()->randomElement([
+                'lead',
+                'secondary',
+                'category_lead',
+                'guides_lead',
+                'important_now',
+            ]),
+            'context_key' => null,
+            'position' => 0,
+            'article_id' => ContentArticle::factory(),
+            'starts_at' => null,
+            'ends_at' => null,
+            'created_by_user_id' => null,
+            'updated_by_user_id' => null,
+        ];
+    }
+}
