46e8745d4b5947f73351cae48044db23f5225182
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-16 10:38:51 +0200

    feat: add newsroom category admin resource

---
+++ app/Filament/Resources/ContentCategories/Pages/CreateContentCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\ContentCategories\Pages;
+
+use App\Filament\Resources\ContentCategories\ContentCategoryResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateContentCategory extends CreateRecord
+{
+    protected static string $resource = ContentCategoryResource::class;
+
+    public function getHeading(): string
+    {
+        return 'Dodaj kategorię newsroomu';
+    }
+}
