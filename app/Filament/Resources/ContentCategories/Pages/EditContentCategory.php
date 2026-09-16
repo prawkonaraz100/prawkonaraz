c9fbe34052750c72aa2a45e2364412797a45b18c
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-16 09:56:16 +0200

    feat: add content category resource page

---
+++ app/Filament/Resources/ContentCategories/Pages/EditContentCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\ContentCategories\Pages;
+
+use App\Filament\Resources\ContentCategories\ContentCategoryResource;
+use App\Models\ContentCategory;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditContentCategory extends EditRecord
+{
+    protected static string $resource = ContentCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            ViewAction::make(),
+            DeleteAction::make()
+                ->disabled(fn (ContentCategory $record): bool => ! $record->canBeDeleted()),
+        ];
+    }
+}
