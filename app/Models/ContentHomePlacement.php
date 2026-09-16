48bbad113e224c5bf3ea409d6c0e6ae313278701
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-16 06:29:51 +0200

    style: order home placement imports

--- app/Models/ContentHomePlacement.php
+++ app/Models/ContentHomePlacement.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use DateTimeInterface;
 use Database\Factories\ContentHomePlacementFactory;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
