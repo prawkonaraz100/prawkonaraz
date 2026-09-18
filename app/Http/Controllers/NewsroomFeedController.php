3e3ef4f53b03aa04b8de94ce19fa25056af98778
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-18 15:19:12 +0200

    feat(newsroom): serve conditional Atom feed

---
+++ app/Http/Controllers/NewsroomFeedController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Support\NewsroomFeedService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
+
+class NewsroomFeedController extends Controller
+{
+    public function __invoke(Request $request, NewsroomFeedService $feedService): Response
+    {
+        $feed = $feedService->build();
+
+        if ($feed === null) {
+            abort(404);
+        }
+
+        $xml = $feedService->render($feed);
+        $response = response($xml, Response::HTTP_OK);
+
+        $response->headers->set('Content-Type', 'application/atom+xml; charset=UTF-8');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->setEtag(hash('sha256', $xml));
+        $response->setLastModified(Carbon::parse($feed['updated'])->utc());
+        $response->setPublic();
+        $response->setMaxAge(max(1, (int) config('newsroom.cache_ttl_seconds', 60)));
+        $response->isNotModified($request);
+
+        return $response;
+    }
+}
